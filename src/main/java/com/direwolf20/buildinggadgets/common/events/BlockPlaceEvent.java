46a8f65de272514dde28ae397940bac3c1aa0aec
ProjectET <projectet@hotmail.com>
2021-09-30 09:34:38 +0930

    BlockPlaceEvent stuff

--- src/main/java/com/direwolf20/buildinggadgets/common/events/BlockPlaceEvent.java
+++ src/main/java/com/direwolf20/buildinggadgets/common/events/BlockPlaceEvent.java
@@ -3,23 +3,26 @@ package com.direwolf20.buildinggadgets.common.events;
 import net.fabricmc.fabric.api.event.Event;
 import net.fabricmc.fabric.api.event.EventFactory;
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.Nullable;
 
 public class BlockPlaceEvent {
 
     private BlockPlaceEvent() {}
 
-    public static final Event<Place> ON_PLACE = EventFactory.createArrayBacked(Place.class, (listeners) -> ((world, entity, pos, state, itemStack) -> {
+    public static final Event<Place> ON_PLACE = EventFactory.createArrayBacked(Place.class, (listeners) -> ((serverPlayer, level, itemStack, interactionHand, blockHitResult) -> {
         for(Place event : listeners) {
-            event.OnPlace(world, entity, pos, state, itemStack);
+            event.OnPlace(serverPlayer, level, itemStack, interactionHand, blockHitResult);
         }
     }));
 
     public interface Place {
-        void OnPlace(Level world, @Nullable /* Nullable */ LivingEntity entity, BlockPos pos, BlockState state, ItemStack itemStack);
+        void OnPlace(ServerPlayer serverPlayer, Level level, ItemStack itemStack, InteractionHand interactionHand, BlockHitResult blockHitResult);
     }
 }
--- src/main/java/com/direwolf20/buildinggadgets/common/mixins/BlockPlaceEventMixin.java
+++ src/main/java/com/direwolf20/buildinggadgets/common/mixins/BlockPlaceEventMixin.java
@@ -2,22 +2,28 @@ package com.direwolf20.buildinggadgets.common.mixins;
 
 import com.direwolf20.buildinggadgets.common.events.BlockPlaceEvent;
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.level.ServerPlayerGameMode;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(Block.class)
+@Mixin(ServerPlayerGameMode.class)
 public class BlockPlaceEventMixin {
 
-    @Inject(method = {"setPlacedBy(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/item/ItemStack;)V"}, at = @At("TAIL"))
-    public void setPlacedBy(Level level, BlockPos blockPos, BlockState blockState, @Nullable LivingEntity livingEntity, ItemStack itemStack, CallbackInfo ci) {
-        BlockPlaceEvent.ON_PLACE.invoker().OnPlace(level, livingEntity, blockPos, blockState, itemStack);
+    @Inject(method = {"useItemOn(Lnet/minecraft/server/level/ServerPlayer;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/InteractionHand;Lnet/minecraft/world/phys/BlockHitResult;)Lnet/minecraft/world/InteractionResult;"}, at = @At("HEAD"))
+    public void setPlacedBy(ServerPlayer serverPlayer, Level level, ItemStack itemStack, InteractionHand interactionHand, BlockHitResult blockHitResult, CallbackInfoReturnable<InteractionResult> cir) {
+        BlockPlaceEvent.ON_PLACE.invoker().OnPlace(serverPlayer, level, itemStack, interactionHand, blockHitResult);
     }
 }
