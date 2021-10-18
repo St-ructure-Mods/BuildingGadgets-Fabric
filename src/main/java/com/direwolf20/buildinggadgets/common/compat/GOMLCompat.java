c8ca8186a0a74cbdc1c8a197643b590f757227f7
ProjectET <projectet@hotmail.com>
2021-10-19 10:28:38 +1030

    fix goml compat

--- src/main/java/com/direwolf20/buildinggadgets/common/compat/GOMLCompat.java
+++ src/main/java/com/direwolf20/buildinggadgets/common/compat/GOMLCompat.java
@@ -1,11 +1,14 @@
 package com.direwolf20.buildinggadgets.common.compat;
 
+import com.jamieswhiteshirt.rtree3i.Entry;
+import draylar.goml.api.Claim;
+import draylar.goml.api.ClaimBox;
 import draylar.goml.api.ClaimUtils;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.Level;
+
+import java.util.stream.Collectors;
 
 public class GOMLCompat {
 
@@ -13,7 +16,9 @@ public class GOMLCompat {
 
     public static boolean canUse(ServerPlayer player, BlockPos pos) {
         if (MOD_LOADED) {
-            return ClaimUtils.getClaimsAt(player.level, pos).anyMatch(entry -> ClaimUtils.playerHasPermission(entry, player));
+            for(Entry<ClaimBox, Claim> entry : ClaimUtils.getClaimsAt(player.level, pos).collect(Collectors.toList())) {
+                return ClaimUtils.playerHasPermission(entry, player);
+            }
         }
 
         return true;
